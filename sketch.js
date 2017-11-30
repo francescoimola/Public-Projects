84f054ed2c30635c9a6a7539efbd6ef5826da93a
francescoimola <33965523+francescoimola@users.noreply.github.com>
2017-11-30 14:46:46 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
+var words = [ "apple", "bear", "cat", "dog" ];
+var word = random(words);
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-	rotate(HALF_PI/3.0);
-	scale(8, 8);
-	rect(70, 5, 60, 100);
+	background(windowWidth, windowHeight);
+  var words = [ "drawings", "x", "drawings", "x" ];
+  var word = random(words);
+  var word2 = random(words);
+  frameRate(10)
+  textSize(50);
+  text(word,38,55);
+  text(word2, 120,55);
+  text(word2, 38,90);
+  text(word, 120,90);
+  strokeWeight(3);
+  scale(1.5, 1.5);
+  line(400, 20, 340, 170);
+  line(340, 170, 410, 220);
 }
 
 function windowResized() {
